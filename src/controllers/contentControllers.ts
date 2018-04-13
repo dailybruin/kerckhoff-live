c5fac6ffc94d78d3470f45df9df3f827df56a2b4
kai <kaileymon@gmail.com>
2018-04-12 22:55:52 -0700

    test

--- src/controllers/contentControllers.ts
+++ src/controllers/contentControllers.ts
@@ -9,5 +9,13 @@ a) create a new KerckhoffContent and add it to the cache if it does not exist
 b) call the update method on the KerckhoffContent if it does
 */
 export function updateController(req: Request, res: Response) {
+  /*slug = req.params.id;
+  if(if KerckhoffContent does not exists) {
+  //Create KerckhoffContent
+  //Add it to the cache
+  }
+  else {
+  //Call update method on KerckhoffContent
+  }*/
   return;
 }
